1c46a9e26ac313d0716f2b0264f9d592e3fa32f2
MissMurigi <vickymurigi001@gmail.com>
2024-07-26 17:03:50 +0300

    filter

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution here
+//code your solutions here 
+function findMatching(drivers, attribute) {
+  return drivers.filter(function (driver) {
+    return driver.toLowerCase() === attribute.toLowerCase();
+  });
+}
+
+function fuzzyMatch(drivers, attribute) {
+  return drivers.filter(function (driver) {
+    return (
+      driver.toLowerCase().substring(0, attribute.length) ===
+      attribute.toLowerCase()
+    );
+  });
+}
+
+function matchName(drivers, argument) {
+  return drivers.filter(function (driver) {
+    return driver.name === argument;
+  });
+}
